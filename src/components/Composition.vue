97f98779e19ece8a42e8bed40070e40bbc8a872d
capture-0 <amin.9800.aa3@gmail.com>
2023-07-16 15:05:51 +0330

    first commit

---
+++ src/components/Composition.vue
@@ -0,0 +1,18 @@
+<template>
+    Composition {{ a }}
+    <button @click="add">add</button>
+</template>
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup(props, ctx) {
+        console.log(ctx);
+        const a = ref(2);
+        return { a }
+    },
+    data() {
+        
+    }
+}
+</script>
